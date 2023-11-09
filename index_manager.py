0e46278e41203adf7cc5bbd96947ecac678ce4c2
Sanket Mishra <isanketmishra@gmail.com>
2023-11-09 15:11:32 -0800

    Revert "fixed initiate index"

--- index_manager.py
+++ index_manager.py
@@ -6,7 +6,7 @@ import pinecone
 import yaml
 from llama_index.llms import OpenAI
 from llama_index.vector_stores import PineconeVectorStore
-from llama_index import GPTVectorStoreIndex, StorageContext, ServiceContext, download_loader, Response, VectorStoreIndex
+from llama_index import GPTVectorStoreIndex, StorageContext, ServiceContext, download_loader, Response
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.prompts import PromptTemplate
 
@@ -24,15 +24,28 @@ class IndexManager:
         self.index_name = config['index_name']
         self.initialize_index()
 
-    def initialize_index(self):        
-        #connect to the existing pinecone index
-        pinecone_index = pinecone.Index(self.index_name)
-        
-        #create a PineconeVectorStore instance
-        vector_store = PineconeVectorStore(pinecone_index)
+    def initialize_index(self):
+        if self.index_name not in pinecone.list_indexes():
+            pinecone.create_index(self.index_name, dimension=1536, metric='cosine')
 
-        #create a VectorStoreIndex instance
-        self.index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+        pinecone_index = pinecone.Index(self.index_name)
+        self.vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+
+        self.storage_context = StorageContext.from_defaults(vector_store=self.vector_store)
+        self.embed_model = OpenAIEmbedding(model='text-embedding-ada-002')
+        self.chunk_size = 256
+        self.chunk_overlap = 100
+        self.service_context = ServiceContext.from_defaults(embed_model=self.embed_model, chunk_size=self.chunk_size, chunk_overlap = self.chunk_overlap)
+
+        integration_token = os.getenv("NOTION_INTEGRATION_TOKEN")
+        NotionPageReader = download_loader('NotionPageReader')
+        page_ids = ["87b45a97333c40eea3d53c1ea8f983cb"]
+        reader = NotionPageReader(integration_token=integration_token)
+        documents = reader.load_data(page_ids=page_ids)
+
+        self.index = GPTVectorStoreIndex.from_documents(documents,
+                                                        storage_context=self.storage_context,
+                                                        service_context=self.service_context)
 
     def get_response(self, question):
         template = (
