ba013b3711c2a0f38f43ad063f4b330b7c02610d
Momo Lapu <momolapu@MacBook-Pro-5.hsd1.ca.comcast.net>
2023-11-12 11:43:46 -0800

    Add writeback to Notion

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
 
 from index_manager import IndexManager
 
 manager = IndexManager()
 app = FastAPI()
+
+
+# Model for request body
+class WriteRequest(BaseModel):
+    query: str
+    answer: str
+
 
 # CORS setup
 origins = ["*"]
@@ -19,17 +27,21 @@ app.add_middleware(
 )
 
 
-@app.get("/{index}/process")
-async def process_data(index: str, data_request: str):
-    response = manager.process(index, data_request)
-    return {"response": response}
-
-
 @app.get("/{index}/respond")
 async def get_response(index: str, question: str):
     response = manager.get_response(index, question)
     # Convert non-serializable parts to serializable format
     return {"response": response}
+
+
+@app.post("/{index}/write/")
+async def write(index: str = Path(..., description="Index to write to"),
+                request: WriteRequest = None):
+    if request is None:
+        raise HTTPException(status_code=400, detail="Request body is required")
+
+    response = manager.write_to_notion(index, request.query, request.answer)
+    return {"status": "write successful"}
 
 
 @app.get("/", tags=["Root"])
--- index_manager.py
+++ index_manager.py
@@ -5,41 +5,26 @@ from dataclasses import dataclass
 import openai
 import pinecone
 import yaml
+from llama_index import GPTVectorStoreIndex, Response
 from llama_index.llms import OpenAI
-from llama_index.vector_stores import PineconeVectorStore
-from llama_index import GPTVectorStoreIndex, StorageContext, ServiceContext, Response, NotionPageReader
-from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.prompts import PromptTemplate
+from llama_index.vector_stores import PineconeVectorStore
+from notion_client import Client
 
 
 @dataclass
 class IndexConfig:
     name: str
     pinecone_api_key: str
+    write_to_page: str
     page_ids: []
 
 
 def initialize_index(index_config):
     pinecone.init(api_key=index_config.pinecone_api_key, environment="gcp-starter")
-    index_name = index_config.name
-
-    if index_name not in pinecone.list_indexes():
-        pinecone.create_index(index_name, dimension=1536, metric='cosine')
-
-    pinecone_index = pinecone.Index(index_name)
-    vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
-
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    service_context = ServiceContext.from_defaults(embed_model=OpenAIEmbedding(model='text-embedding-ada-002'),
-                                                   chunk_size=256,
-                                                   chunk_overlap=100)
-
-    reader = NotionPageReader()
-    documents = reader.load_data(page_ids=index_config.page_ids)
-
-    return GPTVectorStoreIndex.from_documents(documents,
-                                              storage_context=storage_context,
-                                              service_context=service_context)
+    pinecone_index = pinecone.Index(index_config.name)
+    vector_store = PineconeVectorStore(pinecone_index)
+    return GPTVectorStoreIndex.from_vector_store(vector_store=vector_store)
 
 
 class IndexManager:
@@ -52,12 +37,15 @@ class IndexManager:
 
         indexes = config.get('indexes', [])
         self.indexMap = {}
+        self.writePageMap = {}
 
         for index in indexes:
             index_config: IndexConfig = IndexConfig(index.get('name', ''),
                                                     index.get('pinecone_api_key', ''),
+                                                    index.get('write_to_page', ''),
                                                     index.get('page_ids', []))
             self.indexMap[index.get('name', '')] = initialize_index(index_config)
+            self.writePageMap[index.get('name', '')] = index.get('write_to_page', '')
 
     def get_response(self, index, question):
         template = (
@@ -86,40 +74,28 @@ class IndexManager:
         print(res)
         return res.response
 
-    def process(self, index, user_query):
-        openAI_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system",
-                 "content": "You are a helpful assistant, skilled in extracting information from an email. "
-                            "Remove the html and extract the text from this."},
-                {"role": "user", "content": user_query}
-            ]
-
-        )
-        print(openAI_response)
-
-        email_question = openAI_response.choices[0].message["content"]
-        template = (
-            "We have provided context information below. \n"
-            "---------------------\n"
-            "{context_str}"
-            "\n---------------------\n"
-            "Given this information, please answer the question: {query_str}.\n"
-            "If the context provides insufficient information and the question cannot be directly answered, "
-            "reply 'I am lacking knowledge to answer this questions. "
-            "Please enter manually then add it to knowledge base.'"
-        )
-
-        qa_template = PromptTemplate(template)
-        if index is None:
+    def write_to_notion(self, index, query, answer):
+        page_id = self.writePageMap[index]
+        if page_id is None:
             return f"index '{index} not found!'"
 
-        query_engine = index.as_query_engine(text_qa_template=qa_template,
-                                             response_mode='refine', llm=OpenAI(model="gpt-3.5-turbo"))
-
-        start_time = time.time()
-        res: Response = query_engine.query(email_question)
-        print(f"time taken: {(time.time() - start_time)}seconds")
-        print(res)
-        return res.response
+        client = Client(auth=os.environ.get('NOTION_INTEGRATION_TOKEN'))
+        client.blocks.children.append(
+            block_id=page_id,
+            children=[
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "rich_text": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": query + ":" + answer
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        )
